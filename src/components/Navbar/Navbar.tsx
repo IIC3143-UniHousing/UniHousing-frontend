84c74b8a40320b0ea43f36afa9f96f63d4b19f38
Amandasandov <amanda.sandoval@uc.cl>
2025-06-08 19:03:50 -0400

    Feat: integracion de vista de propiedades con el backend

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -40,9 +40,11 @@ const Navbar = ({ user }) => {
             </Link>
             )}
 
-            <Link to="/housings" className={`text-white px-3 py-2 text-sm font-medium ${linkStyle('/residencias')}`}>
-            Ver Residencias
-            </Link>
+            {user && (
+                <Link to="/housings" className={`text-white px-3 py-2 text-sm font-medium ${linkStyle('/residencias')}`}>
+                    Ver Residencias
+                </Link>
+            )}
 
             {user ? (
                 <button 
