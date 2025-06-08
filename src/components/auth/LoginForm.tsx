2ee00ba871d059d26fc7d3a50047d0b061c421e9
rolivares2 <rolivares2@uc.cl>
2025-06-07 21:09:02 -0400

    change in backend api route

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -40,7 +40,7 @@ const LoginForm = () => {
         setSuccess(null);
 
         try {
-            const res = await fetch('http://localhost:3000/api/auth/login', {
+            const res = await fetch('http://localhost:3000/api/users/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json'},
                 body: JSON.stringify({
