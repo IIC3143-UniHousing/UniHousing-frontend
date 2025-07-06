22835ef34557308bdbf7f656b8262d059742c931
CRISTIAN SÁEZ <cssaez@uc.cl>
2025-07-06 14:55:40 -0400

    Fixed login form

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -56,7 +56,7 @@ const LoginForm = () => {
         setSuccess(null);
 
         try {
-            const res = await fetch('http://localhost:3000/api/users/login', {
+            const res = await fetch(import.meta.env.VITE_BACKEND_URL + '/api/users/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json'},
                 body: JSON.stringify({
